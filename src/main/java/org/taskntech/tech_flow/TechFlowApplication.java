1634ce528cf5b2cb9fff2affd0ac90dbee23b332
Katherine <43319584+kperryr@users.noreply.github.com>
2024-12-07 15:27:53 -0500

    added spring boot file( edited dependencies and applications) and connection to mysql successful

---
+++ src/main/java/org/taskntech/tech_flow/TechFlowApplication.java
@@ -0,0 +1,13 @@
+package org.taskntech.tech_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/taskntech/tech_flow/TechFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taskntech.tech_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
