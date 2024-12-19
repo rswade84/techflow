40058e6fef67089ed84c451598a20dd8afe46151
Doug Hirlinger <doughirlinger@gmail.com>
2024-12-19 09:08:43 -0500

    initial security config github login code added

--- src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
+++ src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
@@ -1,4 +1,33 @@
 package org.taskntech.tech_flow.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity(debug = true) //this annotation exposes SecurityFilterChain bean.
 public class SecurityConfig {
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector).servletPath("/");
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        return http
+                .authorizeHttpRequests( auth -> {
+                    //auth.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll();
+                    auth.requestMatchers("/").permitAll(); //(mvc.pattern("/my/controller/**")).hasAuthority("controller");
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(form -> form.defaultSuccessUrl("/", true))
+                //.formLogin(withDefaults()) //form -> form.defaultSuccessUrl("/", true)
+                .build();
+    }
+
 }
