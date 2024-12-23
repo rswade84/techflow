e0d149e4e80dd5ec9a5fca793e05624242148fe9
Doug Hirlinger <doughirlinger@gmail.com>
2024-12-23 09:53:03 -0500

    added un-styled index.html template with fragment head. after login prints all principal attributes

--- src/main/java/org/taskntech/tech_flow/models/Ticket.java
+++ src/main/java/org/taskntech/tech_flow/models/Ticket.java
@@ -3,6 +3,7 @@ package org.taskntech.tech_flow.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotBlank;
 
