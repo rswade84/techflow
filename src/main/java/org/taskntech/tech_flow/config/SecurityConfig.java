502185b071e0162d8a18e434b30d95a7264c2a92
katherinehanra <Kchanra6@gmail.com>
2025-01-01 19:03:32 -0600

    Trying to get OAuth API Running - Getting Sign-In Errors

--- src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
+++ src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
@@ -1,4 +1,35 @@
+//Imported from Doug's OAuth feature branch to run his portion of the app - not original code
 package org.taskntech.tech_flow.config;
 
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity(debug = true) //this annotation exposes SecurityFilterChain bean.
 public class SecurityConfig {
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector).servletPath("/");
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll();
+                    auth.requestMatchers("/", "/login", "/error").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(form -> form.defaultSuccessUrl("/", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .build();
+    }
+
 }
