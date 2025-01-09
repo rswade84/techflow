64b74c226aa5c89a13c4f3e4eac07f3fd398a108
Renaldo <renaldowade@gmail.com>
2025-01-09 12:17:13 -0600

    Reverted back to original blank SecurityConfig

--- src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
+++ src/main/java/org/taskntech/tech_flow/config/SecurityConfig.java
@@ -1,21 +1,4 @@
 package org.taskntech.tech_flow.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
-        @Bean
-        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                return http
-                        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                        .csrf(csrf -> csrf.disable())
-                        .oauth2Login(oauth2 -> oauth2.disable())  // Add this line
-                        .build();
-        }
 }
