eb8689da14d1ab5fc1639304f5a0304ecae9f204
Renaldo <renaldowade@gmail.com>
2025-01-25 13:40:57 -0600

    Added ResponseTimeMetrics.java in the models folder.

---
+++ src/main/java/org/taskntech/tech_flow/models/ResponseTimeMetrics.java
@@ -0,0 +1,14 @@
+package org.taskntech.tech_flow.models;
+
+
+public class ResponseTimeMetrics {
+
+        // Setup response time metrics with average initial response time and average resolution time
+        private double averageInitialResponseTime;
+        private double averageResolutionTime;
+
+
+
+
+
+}
